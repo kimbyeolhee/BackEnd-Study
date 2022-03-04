d3a84f2579cea91fd16751197fe27ee65569ca6c
Byeolhee-Kim <42535803+kimbyeolhee@users.noreply.github.com>
2022-03-04 18:56:03 +0900

    Add files via upload

---
+++ NODE-JS/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://byeolhee:201818@kbh.rcun4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
---
+++ NODE-JS/config/prod.js
@@ -0,0 +1,5 @@
+// heroku 같이 배포할 때 사용하는 파일
+
+module.exports = {
+  mongoURI: process.env.MONGO_URI
+}
